11c69f5e037cfb4faebc79a57dd88622f3ada9ec
Simone <bertoni.simone@gmail.com>
2024-08-10 15:16:55 +0100

    Add noise plot

--- plot_results.m
+++ plot_results.m
@@ -12,18 +12,21 @@ t_omega_est = out.logsout.get('omega_est').Values.Time;
 theta = out.logsout.get('theta').Values.Data(:);
 t_theta = out.logsout.get('theta').Values.Time;
 
-theta_est = out.logsout.get('theta_est').Values.Data;
+theta_est = out.logsout.get('theta_est').Values.Data(:);
 t_theta_est = out.logsout.get('theta_est').Values.Time;
 
 theta_meas = out.logsout.get('theta_meas').Values.Data;
 t_theta_meas = out.logsout.get('theta_meas').Values.Time;
+
+noise = out.logsout.get('noise').Values.Data;
+t_noise = out.logsout.get('noise').Values.Time;
 
 
 % Create the first figure
 figure;
 
 % Subplot for omega
-subplot(2, 1, 1);
+subplot(3, 1, 1);
 plot(t_omega, omega, 'LineWidth', 2);
 hold on;
 plot(t_omega_est, omega_est, 'LineWidth', 2);
@@ -35,14 +38,21 @@ set(gca, 'FontSize', 12);
 grid on;
 
 % Subplot for theta
-subplot(2, 1, 2);
-plot(t_theta, theta*pi/180, 'LineWidth', 2);
+subplot(3, 1, 2);
+plot(t_theta, theta*180/pi, 'LineWidth', 2);
 hold on;
-plot(t_theta_meas, theta_meas*pi/180, 'LineWidth', 2);
-plot(t_theta_est, theta_est*pi/180, 'LineWidth', 2);
+plot(t_theta_meas, theta_meas*180/pi, 'LineWidth', 2);
+plot(t_theta_est, theta_est*180/pi, 'LineWidth', 2);
 hold off;
-xlabel('Time (s)');
 ylabel('Angular position - \theta (deg)');
 legend({'Real', 'Measured', 'Estimated by the Kalman Filter'}, 'FontSize', 12);
+set(gca, 'FontSize', 12);
+grid on;
+
+% Subplot for noise
+subplot(3, 1, 3);
+plot(t_noise, noise*180/pi, 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Measurement noise (deg)');
 set(gca, 'FontSize', 12);
 grid on;
